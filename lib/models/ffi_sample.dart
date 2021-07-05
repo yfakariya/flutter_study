9c14ec196b4a261218529687aecf1d6d0a4f904c
yfakariya <yfakariya@gmail.com>
2021-07-05 20:55:28 +0900

    Implement FormatMessage

--- lib/models/ffi_sample.dart
+++ lib/models/ffi_sample.dart
@@ -15,6 +15,43 @@ void _ensureFfiInitialized() {
   }
 }
 
+String _formatMessage(int win32Error) {
+  const int flags = FORMAT_MESSAGE_IGNORE_INSERTS |
+      FORMAT_MESSAGE_FROM_SYSTEM |
+      FORMAT_MESSAGE_ARGUMENT_ARRAY |
+      0x00000100; // FORMAT_MESSAGE_ALLOCATE_BUFFER;
+  // LPTSTR* ppBuffer, which is pointer to the buffer  to store an pointer to allocated Utf16 string.
+  final ppBuffer = calloc.allocate<IntPtr>(sizeOf<IntPtr>());
+  try {
+    final usedLength = FormatMessage(
+      flags,
+      nullptr,
+      win32Error,
+      0, // dwLangId(0) means current thread locale
+      ppBuffer.cast<Utf16>(),
+      0,
+      nullptr,
+    );
+
+    // Get LPTSTR value (*(LPTSTR*)).
+    final pBuffer = Pointer<Utf16>.fromAddress(ppBuffer.value);
+    try {
+      if (usedLength > 0) {
+        return pBuffer.toDartString(length: usedLength);
+      }
+
+      final formatMessageError = GetLastError();
+      return 'Failed to call FormatMessage with error code ${formatMessageError} (0x${formatMessageError.toHexString(32)})';
+    } finally {
+      // LPTSTR pBuffer value was allocated in FormatMessage, so we must free it via LocalFree function.
+      LocalFree(pBuffer.address);
+    }
+  } finally {
+    // Frees LPTSTR* buffer.
+    calloc.free(ppBuffer);
+  }
+}
+
 String doTest(String filePath) {
   _ensureFfiInitialized();
 
@@ -23,7 +60,7 @@ String doTest(String filePath) {
     pFilePath,
     FILE_READ_ACCESS,
     FILE_SHARE_READ,
-    Pointer.fromAddress(NULL),
+    nullptr,
     OPEN_EXISTING,
     0,
     NULL,
@@ -31,9 +68,9 @@ String doTest(String filePath) {
 
   final error = GetLastError();
   if (hFile == INVALID_HANDLE_VALUE) {
-    return 'Failed to open file "$filePath". Error is ${error}.';
+    return 'Failed to open file "$filePath". ${_formatMessage(error)} ($error)';
   }
 
   final success = CloseHandle(hFile);
-  return 'Succeeded to open file "$filePath". Error was ${error}, CloseHandle returns ${success}';
+  return 'Succeeded to open file "$filePath". Error was "${_formatMessage(error)}" (${error}), CloseHandle returns ${success}';
 }
